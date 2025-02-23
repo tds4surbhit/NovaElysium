b85f094be51d737fcc42f976a7023531efa36f07
tds4surbhit <surbhit.4zeracing@gmail.com>
2025-02-23 18:07:19 +0530

    Adding CatergoryService

---
+++ novaElysium/src/main/java/com/novaElysium/carts/exceptions/AlreadyExistsException.java
@@ -0,0 +1,6 @@
+package com.novaElysium.carts.exceptions;
+
+public class AlreadyExistsException extends  RuntimeException{
+    public AlreadyExistsException(String message) {super (message);}
+
+}
