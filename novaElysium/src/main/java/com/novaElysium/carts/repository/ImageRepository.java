bf03a2245f44b6aa627ae13f016ddee3d0df0c57
tds4surbhit <surbhit.4zeracing@gmail.com>
2025-03-07 03:54:59 +0530

    Adding Image Controller

--- novaElysium/src/main/java/com/novaElysium/carts/repository/ImageRepository.java
+++ novaElysium/src/main/java/com/novaElysium/carts/repository/ImageRepository.java
@@ -1,6 +1,7 @@
 package com.novaElysium.carts.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.novaElysium.carts.model.Image;
 
 public interface ImageRepository extends JpaRepository<Image , Long> {
 }
--- novaElysium/src/main/java/com/novaElysium/carts/service/image/IImageService.java
+++ novaElysium/src/main/java/com/novaElysium/carts/service/image/IImageService.java
@@ -1,15 +1,17 @@
 package com.novaElysium.carts.service.image;
 
+import com.novaElysium.carts.dto.ImageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import com.novaElysium.carts.model.Image;
+import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public interface IImageService  {
     Image getImageById(Long id);
     void deleteImageById(Long id);
-    Image saveImage(MultipartFile file, Long productId);
+    List<ImageDto> saveImages(List<MultipartFile> files, Long productId);
     void updateImage(MultipartFile file, Long imageId);
 
 }
